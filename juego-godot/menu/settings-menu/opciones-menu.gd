4f96ab21afbefeefff57a80ff16c2f7ad0e01596
Operacion Libertad <operacionlibertad.2023@gmail.com>
2023-10-24 22:38:16 -0300

    Add files via upload

---
+++ juego-godot/menu/settings-menu/opciones-menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+func _on_opcionesaudio_button_down():
+	get_tree().change_scene_to_file("res://menu-settibngs-audio/src/sample_scene/sample_scene.tscn")
+
+
+func _on_opcionesvideo_button_down():
+	get_tree().change_scene_to_file("res://menu-settings-video/menu-video-settings.tscn")
+
+
+func _on_salir_button_down():
+	get_tree().change_scene_to_file("res://menu/menu.tscn")
+
+
+func _on_reanudarmenu_button_down():
+	get_tree().change_scene_to_file("res://Personajes/personaje.tscn")
